907166328cd2d18b66a631d4bbc4127c33171081
Keshab Aryal <kesab.nipunasewa@gmail.com>
2023-01-14 20:45:41 +0545

    app name

--- lib/repository/constants/constants.dart
+++ lib/repository/constants/constants.dart
@@ -1,1 +1,1 @@
-const kAppName = " Hamro Khata";
+const kAppName = "Car App";
--- lib/repository/services/base_service.dart
+++
@@ -1,5 +0,0 @@
-abstract class BaseService {
-  final String carBaseUrl = "assets/json_car_data.json";
-
-  Future<dynamic> getResponse(String url);
-}
